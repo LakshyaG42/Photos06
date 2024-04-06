8fb3e3d676e44ae69bbf7f185740783473ca951e
Dhruv Shidhaye <dsshidhaye@gmail.com>
2024-04-06 09:03:49 -0400

    album controller setup

---
+++ Photos/src/photosfx/controllers/albumController.java
@@ -0,0 +1,26 @@
+package photosfx.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionModel;
+import javafx.scene.control.ListCell;
+import javafx.scene.Node;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.Image;
+import javafx.scene.text.Text;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+
+import java.io.IOException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.List;
+
+import photosfx.models.User;
+import photosfx.models.Album;
+import photosfx.models.Photo;
+
+public class albumController {
+    
+}
