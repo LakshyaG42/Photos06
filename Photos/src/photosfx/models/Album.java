8fad31f8ae96e5a477378c518d1f70b2ea9bdbe3
Dhruv Shidhaye <dsshidhaye@gmail.com>
2024-04-06 09:53:36 -0400

    rand

--- Photos/src/photosfx/models/Album.java
+++ Photos/src/photosfx/models/Album.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Album implements Serializable {
+    
     private static final long serialVersionUID = 1L;
 
     private String name;
