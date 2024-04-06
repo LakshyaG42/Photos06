b30bbf6b4519ff4f85da5fcc88b5266925a4b49e
LakshyaG42 <78310209+LakshyaG42@users.noreply.github.com>
2024-04-05 22:17:50 -0400

    Admin and Photo Code

--- Photos/src/photosfx/models/Admin.java
+++ Photos/src/photosfx/models/Admin.java
@@ -1,5 +1,50 @@
 package photosfx.models;
 
+import java.util.List;
+
 public class Admin {
-    
+    private static List<User> users;
+
+    public static void listUsers() {
+        for(User user : users) {
+            System.out.println(user.getUsername());
+        }
+    }
+
+    public static void createUser(String username) {
+        if(username == null || username.isEmpty()) {
+            throw new IllegalArgumentException("Username cannot be null or empty");
+        }
+        for(User user : users) {
+            if(user.getUsername().equals(username)) {
+                throw new IllegalArgumentException("Username already exists");
+            }
+        }
+        User newUser = new User(username);
+        users.add(newUser);
+    }
+
+    public static void deleteUser(String username) {
+        if(username == null || username.isEmpty()) {
+            throw new IllegalArgumentException("Username cannot be null or empty");
+        }
+        User userToDelete = null;
+        for(User user : users) {
+            if(user.getUsername().equals(username)) {
+                userToDelete = user;
+                break;
+            }
+        }
+        if(userToDelete == null) {
+            throw new IllegalArgumentException("User does not exist");
+        }
+        users.remove(userToDelete);
+    }
+
+    @Override
+    public String toString() {
+        return "Admin{" +
+                "users=" + users +
+                '}';
+    }
 }
--- Photos/src/photosfx/models/Photo.java
+++ Photos/src/photosfx/models/Photo.java
@@ -1,6 +1,72 @@
 package photosfx.models;
 
-public class Photo {
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
+public class Photo implements Serializable {
+    private static final long serialVersionUID = 1L;
 
+    private String filePath;
+    private LocalDateTime dateTaken; 
+    private String caption;
+    private Map<String, String> tags; 
+
+    public Photo(String filePath, LocalDateTime dateTaken, String caption) {
+        this.filePath = filePath;
+        this.dateTaken = dateTaken;
+        this.caption = caption;
+        this.tags = new HashMap<>();
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public LocalDateTime getDateTaken() {
+        return dateTaken;
+    }
+
+    public void setDateTaken(LocalDateTime dateTaken) {
+        this.dateTaken = dateTaken;
+    }
+
+    public String getCaption() {
+        return caption;
+    }
+
+    public void setCaption(String caption) {
+        this.caption = caption;
+    }
+
+    public Map<String, String> getTags() {
+        return tags;
+    }
+
+    public void setTags(Map<String, String> tags) {
+        this.tags = tags;
+    }
+
+    public void addTag(String tagName, String tagValue) {
+        tags.put(tagName, tagValue);
+    }
+
+    public void removeTag(String tagName) {
+        tags.remove(tagName);
+    }
+
+    @Override
+    public String toString() {
+        return "Photo{" +
+                "filePath='" + filePath + '\'' +
+                ", dateTaken=" + dateTaken +
+                ", caption='" + caption + '\'' +
+                ", tags=" + tags +
+                '}';
+    }
 }
